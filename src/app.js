a1e05088fd51c0582f526d129f55d48d73cc3092
A. R. Saravia <andresromansaravia@gmail.com>
2025-02-08 18:07:46 -0300

    cleaning up messages files

--- src/app.js
+++ src/app.js
@@ -33,21 +33,38 @@ app.use('/api/products',productRouter);
 app.use('/products',viewsProductRouter);
 app.use('/realtimeproducts',realTimeProductsRouter);
 
-const messages = [];
+let products = [];
 const socketServer = new Server(httpServer);
 socketServer.on('connection', (socket) => {
 	console.log('Nueva conexión.')
 
-    socket.on('message', data  => {
+    socket.on('product', data  => {
         console.log(data);
     });
 
-    socket.emit('loadMessages', messages);
+	socket.emit('loadProducts', products);
 
-    socket.on('newMessage', (message) => {
-        const newMessage = { socketid: socket.id, message }
-        messages.push(newMessage);
-        //console.log(messages);
-        socketServer.emit('newMessage', newMessage);
+    socket.on('newProduct', (product) => {
+        const newProduct = product
+		if (products.length == 0) {
+			product["id"] = "0";
+		}else{
+			product["id"] = (Math.max.apply(Math, products.map((product) => product.id))+1).toString()
+		}
+        products.push(newProduct);
+        console.log(products);
+        socketServer.emit('newProduct', newProduct);
+    })
+
+    socket.on('deleteProduct', (pid) => {
+		const index = products.findIndex(p => p.id === pid.toString())
+		if(index === -1){
+			console.log("Producto no encontrado.");
+		}else{
+			products.splice(index,1);
+			console.log('Producto eliminado exitosamente.');
+		}
+        console.log(products);
+        socketServer.emit('loadProducts', products);
     })
 })
--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -1,34 +1,5 @@
-import ProductManager from "../managers/ProductManager.js";
-const productManager = new ProductManager('src/public/json/products.json');
-
 const socket = io();
-socket.emit('message', 'Me estoy comunicando desde un websocket.');
-
-const messageInput = document.getElementById('messageInput');
-const sendMessageButton = document.getElementById('sendMessage');
-const messageContainer = document.getElementById('messageContainer');
-
-sendMessageButton.addEventListener('click', () => {
-    const message = messageInput.value;
-    socket.emit('newMessage', message);
-    messageInput.value = '';
-})
-
-socket.on('loadMessages', (messages)  => {
-    messages.forEach( (message ) => {
-        cargarMensaje(message);
-    })
-})
-
-socket.on('newMessage', (message)  => {
-    cargarMensaje(message);
-})
-
-function cargarMensaje(message){
-    const messageElement = document.createElement('p');
-    messageElement.textContent = `${message.socketid} : ${message.message}`;
-    messageContainer.appendChild(messageElement);
-}
+socket.emit('product', 'Me estoy comunicando desde un websocket.');
 
 const titleInput = document.getElementById('titleInput');
 const descriptionInput = document.getElementById('descriptionInput');
@@ -39,9 +10,18 @@ const stockInput = document.getElementById('stockInput');
 const categoryInput = document.getElementById('categoryInput');
 const thumbnailsInput = document.getElementById('thumbnailsInput');
 const sendProductButton = document.getElementById('sendProduct');
+const productContainer = document.getElementById('productContainer');
 
+titleInput.value = 'obj1'
+descriptionInput.value = 'd1'
+codeInput.value = 0
+priceInput.value = 0
+statusInput.value = 0
+stockInput.value = 0
+categoryInput.value = ''
+thumbnailsInput.value = ''
 sendProductButton.addEventListener('click', () => {
-    const product = {}
+    let product = {}
 	product['title'] = titleInput.value
 	product['description'] = descriptionInput.value
 	product['code'] = codeInput.value
@@ -50,31 +30,40 @@ sendProductButton.addEventListener('click', () => {
 	product['stock'] = stockInput.value
 	product['category'] = categoryInput.value
 	product['thumbnails'] = thumbnailsInput.value
-	titleInput.value = ''
-	descriptionInput.value = ''
-	codeInput.value = ''
-	priceInput.value = ''
-	statusInput.value = ''
-	stockInput.value = ''
+	titleInput.value = 'obj1'
+	descriptionInput.value = 'd1'
+	codeInput.value = 0
+	priceInput.value = 0
+	statusInput.value = 0
+	stockInput.value = 0
 	categoryInput.value = ''
 	thumbnailsInput.value = ''
     socket.emit('newProduct', product);
 })
 
-socket.on('loadMessages', (messages)  => {
-    messages.forEach( (message ) => {
-        cargarMensaje(message);
+socket.on('loadProducts', (products)  => {
+    products.forEach( (product) => {
+        loadProduct(product);
     })
 })
 
 socket.on('newProduct', (product)  => {
-    cargarMensaje(message);
+    loadProduct(product);
 })
 
-function cargarMensaje(message){
-    const messageElement = document.createElement('p');
-    messageElement.textContent = `${message.socketid} : ${message.message}`;
-    messageContainer.appendChild(messageElement);
+function loadProduct(product){
+    const productElement = document.createElement('p');
+    productElement.textContent = `${product.title} (${product.id}): ${product.description} (${product.code}, ${product.category}) ($${product.price}, ${product.stock} unidades)`;
+	const btn = document.createElement('button');
+	btn.id = product.id
+	btn.textContent = 'Borrar';
+	btn.className = 'delete'
+	btn.type = 'submit'
+	btn.addEventListener('click', () => {
+		console.log("LLEGÓ")
+		socket.emit('deleteProduct', product.id);
+	})
+    productContainer.appendChild(productElement);
+    productContainer.appendChild(btn);
+	console.log(productElement,btn);
 }
-
-
