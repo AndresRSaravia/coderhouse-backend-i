7ba92a6a23d73195d028e64dde88183c156f6f8a
A. R. Saravia <andresromansaravia@gmail.com>
2025-02-24 21:14:59 -0300

    previous start is now index, previous index is now products

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -1,14 +1,9 @@
 import {Router} from 'express';
-import ProductManager from "../public/managers/ProductManager.js";
 
 const router = Router();
-const productManager = new ProductManager('src/public/json/products.json');
 
-// Métodos GET para productos
 router.get('/',(req,res) => {
-	console.log('Pedido viewer de listado de productos.');
-	const products = productManager.readProducts()
-	res.render('products',{products})
+	res.render('index', {})
 });
 
 export default router;
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,58 +1,92 @@
 import {Router} from 'express';
-import ProductManager from "../public/managers/ProductManager.js";
+import productModel from '../models/product.model.js'
 
 const router = Router();
-const productManager = new ProductManager('src/public/json/products.json');
 
 // Métodos GET para productos
-router.get('/',(req,res) => {
+router.get('/', async (req,res) => {
 	console.log('Pedido de listado de productos.');
-	const products = productManager.readProducts()
-	return res.json(products);
-});
-
-router.get('/:pid', function (req, res) {
-	console.log('Id de producto a buscar:', req.params.pid);
-	const foundProduct = productManager.readProduct(req.params.pid);
-	if(!foundProduct){
-		return res.status(404).send({status: "error", error: "Producto no encontrado."});
+	try{
+		const products = await productModel.find();
+		return res.json({products});
+	}catch(error){
+		console.log(error)
+		return res.status(500).send({status: 'error', error: 'Error al obtener el listado de productos.'});
 	}
-	return res.send(foundProduct);
+	/*{
+	status:success/error
+payload: Resultado de los productos solicitados
+totalPages: Total de páginas
+prevPage: Página anterior
+nextPage: Página siguiente
+page: Página actual
+hasPrevPage: Indicador para saber si la página previa existe
+hasNextPage: Indicador para saber si la página siguiente existe.
+prevLink: Link directo a la página previa (null si hasPrevPage=false)
+nextLink: Link directo a la página siguiente (null si hasNextPage=false)
+}
+*/
 });
 
-// Método POST para productos
-router.post('/',(req,res) => {
-	console.log('Pedido de creación de producto.');
-	const newProduct = req.body;
-	const incompleteValues1 = !newProduct.title||!newProduct.description||!newProduct.code||(newProduct.price == null)
-	const incompleteValues2 = (newProduct.status == null)||(newProduct.stock == null)||!newProduct.category||!newProduct.thumbnails
+router.get('/:pid', async (req, res) => {
+	console.log('Id del producto a buscar:', req.params.pid);
+	try{
+		const pid = req.params.pid
+		const foundProduct = await productModel.findOne({_id: pid});
+		console.log(foundProduct)
+		return res.send(foundProduct);
+	}catch(error){
+		console.log(error)
+		return res.status(500).send({status: 'error', error: 'Error al obtener el producto.'});
+	}
+});
+
+// Métodos POST para productos
+router.post('/', async (req,res) => {
+	console.log('Pedido del creación de producto.');
+	console.log('Información del req.body', req.body);
+	const incompleteValues1 = !req.body.title||!req.body.description||!req.body.code||(req.body.price == null)
+	const incompleteValues2 = (req.body.status == null)||(req.body.stock == null)||!req.body.category||!req.body.thumbnail
 	const incompleteValues = incompleteValues1||incompleteValues2
 	if(incompleteValues){
-		return res.status(400).send({status: "error", error: "Valores incompletos."});
+		return res.status(400).send({status: 'error', error: 'Valores incompletos.'});
 	}
-	productManager.createProduct(newProduct);
-	return res.json({message: 'Producto agregado exitosamente.'});
+	try{
+		const newProduct = new productModel(req.body);
+		await newProduct.save();
+		return res.json({status: 'success', message: 'Producto agregado exitosamente.'});
+	}catch(error){
+		console.log(error)
+		return res.status(500).send({status: 'error', error: 'Error al crear un producto.'});
+	}
 });
 
-// Método PUT para productos
-router.put('/:pid', (req,res) => {
-	console.log('Id de producto a actualizar:', req.params.pid);
-	delete req.body.id;
-	const index = productManager.updateProduct(req.params.pid,req.body);
-	if(index === -1){
-		return res.status(404).send({status: "error", error: "Producto no encontrado."})
+// Métodos PUT para productos
+router.put('/:pid', async (req,res) => {
+	delete req.body._id;
+	console.log('Id del producto a actualizar:', req.params.pid);
+	try{
+		const pid = req.params.pid;
+		const updatedProduct = req.body;
+		const result = await productModel.updateOne({_id: pid}, updatedProduct);
+		return res.send({status:'success', message: 'Producto actualizado.', payload: result});
+	}catch(error){
+		console.log(error)
+		return res.status(500).send({status: 'error', error: 'Error al actualizar el producto.'});
 	}
-	return res.send({status: "success", message: "Producto actualizado."});
 });
 
-// Método DELETE para productos
-router.delete('/:pid',(req,res) => {
-	console.log('Id de producto a eliminar:', req.params.pid);
-	const index = productManager.deleteProduct(req.params.pid);
-	if(index == -1){
-		return res.status(404).send({status: "error", error: "Producto no encontrado."});
+// Métodos DELETE para productos
+router.delete('/:pid', async (req,res) => {
+	console.log('Id del producto a eliminar:', req.params.pid);
+	try{
+		const pid = req.params.pid;
+		const result = await productModel.deleteOne({_id: pid});
+		return res.send({status:'success', message: 'Producto eliminado.', payload: result});
+	}catch(error){
+		console.log(error)
+		return res.status(500).send({status: 'error', error: 'Error al borrar el producto.'});
 	}
-    return res.send({status: "success", message: "Producto eliminado."});
 });
 
 export default router;
