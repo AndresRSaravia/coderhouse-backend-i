da336a9443df7ad4408be3cea232fb38a4536d9f
A. R. Saravia <andresromansaravia@gmail.com>
2025-02-24 21:15:49 -0300

    app major update

--- src/app.js
+++ src/app.js
@@ -1,62 +1,53 @@
-import fs from 'fs';
-import ProductManager from "./public/managers/ProductManager.js";
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
 import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
-import startRouter from './routes/start.router.js';
+import indexRouter from './routes/index.router.js';
 import cartRouter from './routes/carts.router.js';
 import productRouter from './routes/products.router.js';
-import viewsProductRouter from './routes/index.router.js';
-import realTimeProductsRouter from './routes/views.router.js';
+import listProductsRouter from './routes/listproducts.router.js';
+import listCartProductsRouter from './routes/listcartproducts.router.js';
 import path from 'path';
-import {Server} from 'socket.io';
 
+// Inicialización server
 const app = express();
-const httpServer = app.listen(8080, () => {
-    console.log("Servidor inicializado. Escuchando."); //Servidor HTTP
-})
 
-// Configurar el motor de plantillas handlebars
+// Acceso al .env
+dotenv.config();
+
+// Inicialización de las variables de entorno
+const URLMONGO = process.env.URLMONGO;
+
+// Conexión a la base de datos
+console.log(URLMONGO)
+mongoose.connect(URLMONGO)
+    .then( () => console.log("Conexión a base de datos exitosa."))
+    .catch( (error) => {
+        console.error("Error en la conexión a la base de datos: ", error);
+        process.exit();
+    });
+
+// Configuración para trabajar con json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// Configuración del motor de plantillas handlebars
 app.engine('handlebars',handlebars.engine())
 app.set('views',path.join(__dirname,'/views'))
 app.set('view engine', 'handlebars')
 
-// Archivos estáticos
+// Configuración para archivos estáticos
 app.use(express.static(path.join(__dirname,'/public')))
 
-// Habilitar lectura de archivos JSON
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// Router
-app.use('/',startRouter); // página inicial
+// Configuración para routers
+app.use('/',indexRouter);
 app.use('/api/carts',cartRouter);
 app.use('/api/products',productRouter);
-app.use('/products',viewsProductRouter);
-app.use('/realtimeproducts',realTimeProductsRouter);
+app.use('/products',listProductsRouter);
+app.use('/cartproducts',listCartProductsRouter);
 
-const productManager = new ProductManager('src/public/json/products.json');
-const socketServer = new Server(httpServer);
-socketServer.on('connection', (socket) => {
-	console.log('Nueva conexión.')
-
-    socket.on('product', data  => {
-        console.log(data);
-    });
-
-	socket.emit('loadProducts', productManager.readProducts());
-
-    socket.on('newProduct', (product) => {
-		productManager.createProduct(product);
-		const products = productManager.readProducts()
-        console.log(products);
-        socketServer.emit('newProduct', product);
-    })
-
-    socket.on('deleteProduct', (pid) => {
-		productManager.deleteProduct(pid);
-		const products = productManager.readProducts()
-        console.log(products);
-    	//socketServer.emit('loadProducts', products);
-    })
-})
+// Inicialización del servidor
+app.listen(8080, () => {
+	console.log("Servidor inicializado. Escuchando.");
+});
